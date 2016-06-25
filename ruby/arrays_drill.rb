390aeb727f7f1c9a2541466f9f7fd282e4508a3b
Milan Cook <milancook@gmail.com>
2016-06-25 18:52:05 -0500

    Started release 1

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,13 @@
+#initialize empty array
+tv_shows = []
+
+#store empty array in a variable
+favorite = tv_shows
+
+#print the variable
+p favorite
+
+#add five items to array
+tv_shows + ["Penny Dreadful", "Game of Thrones", "How To Get Away With Murder", "Outlander", "Orange is the New Black"]
+
+ 
